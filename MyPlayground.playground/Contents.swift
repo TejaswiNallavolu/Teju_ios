79809afc2a521fd1bb95f9ee9b4f5209559a4ebe
TejaswiNallavolu <77799992+TejaswiNallavolu@users.noreply.github.com>
2021-08-19 14:48:54 -0500

    day 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var i = 9
+var j = 11
+var sum = i + j
+print(sum)
+
+var k = 8.9;
+var v = 6.39;
+var product = 0.0;
+product = k * v
+print(product)
+
+var a = 86;
+var b = 6;
+var div = 0.0;
+div = k / v
+print(div)
+
+
+
+
+
